f8af6e2005b6902bde59fd6a73817de001893b84
Romain Barbero <barsat1004@gmail.com>
2022-10-12 15:15:08 +0200

    Initialisation du router et de la db + add assets

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
